23bba01ceb3712810e34c30e42412086e6298acc
Michael Wuergler <wuergler@gmail.com>
2017-03-18 06:57:20 +0000

    rename to lorem.js

---
+++ snippets/lorem.js
@@ -0,0 +1,22 @@
+module.exports = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque quis elit
+et euismod. Suspendisse blandit velit dui, in vulputate lorem bibendum eu.
+Pellentesque nec ullamcorper enim. Integer laoreet quis urna a molestie. Proin
+elit orci, ullamcorper a lorem vel, consectetur accumsan eros. Vestibulum ante
+ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam
+sed aliquet neque. Nulla dignissim fringilla quam, et gravida metus semper nec.
+
+Vestibulum consectetur molestie nunc, nec finibus nisi tempus sagittis.
+In molestie, neque mollis venenatis facilisis, erat ipsum blandit enim, a
+gravida nunc neque at justo. Class aptent taciti sociosqu ad litora torquent
+per conubia nostra, per inceptos himenaeos.
+
+Nullam at placerat mi. Aenean vehicula volutpat justo, eget porttitor nibh
+fermentum et. Ut at interdum urna, at lobortis mi. Cras eu leo metus. Proin
+vulputate augue in quam euismod, sed tristique leo vehicula. Quisque in blandit
+felis. Nunc iaculis iaculis risus, in bibendum purus dignissim sed. Morbi accumsan
+vehicula elit quis volutpat. Nullam interdum, nisl mollis posuere ultricies,
+odio metus facilisis nunc, eget aliquam mi nulla sit amet ante. Nulla accumsan
+quis mauris nec dapibus. Sed condimentum mollis lacinia. Vestibulum commodo
+hendrerit felis, id viverra massa pellentesque.
+`;
